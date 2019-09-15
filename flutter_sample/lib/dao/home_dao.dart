5f1f63ca257349dd175caa9f81316a0978613ab7
Casuy <casuistry016@gmail.com>
2019-09-15 18:17:26 +1000

    udpate

--- flutter_sample/lib/dao/home_dao.dart
+++ flutter_sample/lib/dao/home_dao.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:flutter_sample/model/home_model.dart';
 import 'package:http/http.dart' as http;
 
-const HOME_URL = 'https://raw.githubusercontent.com/Casuy/mobile_frontend/master/flutter_sample/lib/data/home_page.json?token=AJZUEJ2JXRESGXGZBVOXVSS5Q4VES';
+const HOME_URL = 'https://raw.githubusercontent.com/Casuy/mobile_frontend/master/flutter_sample/lib/data/home_page.json?token=AJZUEJ3SNN4MQ7LJMUFTFRC5Q4W4O';
 
 class HomeDao{
   static Future<HomeModel> fetch() async {
